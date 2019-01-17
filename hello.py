e9ef268624a2b1a040559fbda4afae31146deba1
y² <yy@yuyuan.org>
2019-01-17 12:40:36 -0800

    re-organized code lines

--- hello.py
+++ hello.py
@@ -20,20 +20,23 @@ class HelloView(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
 
+        # label
+        name_label = ttk.Label(self, text="Name:")
+        name_label.grid(row=0, column=0, sticky=tk.W)
+
+        # edit box
         self.name = tk.StringVar()
+        name_entry = ttk.Entry(self, textvariable=self.name)
+        name_entry.grid(row=0, column=1, sticky=(tk.W + tk.E))
+
+        # button
+        chg_button = ttk.Button(self, text="Change", command=self.on_change)
+        chg_button.grid(row=0, column=2, sticky=tk.E)
+
+        # label
         self.hello_string = tk.StringVar()
         self.hello_string.set("Hello World")
-
-        name_label = ttk.Label(self, text="Name:")
-        name_entry = ttk.Entry(self, textvariable=self.name)
-
-        ch_button = ttk.Button(self, text="Change", command=self.on_change)
-
         hello_label = ttk.Label(self, textvariable=self.hello_string, font=("TkDefaultFont", 64), wraplength=600)
-
-        name_label.grid(row=0, column=0, sticky=tk.W)
-        name_entry.grid(row=0, column=1, sticky=(tk.W + tk.E))
-        ch_button.grid(row=0, column=2, sticky=tk.E)
         hello_label.grid(row=1, column=0, columnspan=3)
 
         self.columnconfigure(1, weight=1)
@@ -49,6 +52,7 @@ class MyApplication(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
         self.title("Hello Tkinter")
         self.geometry("800x600")
         self.resizable(width=False, height=False)
